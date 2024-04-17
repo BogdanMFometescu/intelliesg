a54af8746ffbba1a1a04f7ff90a5cc846cad9246
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-04-17 20:51:41 +0300

    created urls for pillars and html forms
    created urls for pillars and html forms

--- esgmanager/urls.py
+++ esgmanager/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [path('pillars/', views.ListViewPillars.as_view(), name='pillars')
                path('pillar/<uuid:pk>/', views.DeleteViewPillars.as_view(), name='pillar'),
                path('create-pillar/', views.CreateViewPillars.as_view(), name='create_pillar'),
                path('update-pillar/<uuid:pk>/', views.UpdateViewPillars.as_view(), name='update_pillar'),
-               path('delete-pillar/<uuid:pk>/', views.DeleteViewPillars.as_view(), name='delete_pillar')]
+               path('delete-pillar/<uuid:pk>/', views.DeleteViewPillars.as_view(), name='delete_pillar'),
+               path('esg-nav-form-pillar',views.NavFormPillars.as_view(),name='esg-nav-form-pillar'),
+               path('esg-nav-form-action-plan',views.NavFormActionPlan.as_view(),name='esg-nav-forms-action-plan')]
