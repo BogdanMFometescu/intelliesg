ec738b3f6d96e43d979518d5452db9e040fca227
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-04-17 21:46:09 +0300

    v 0.8.1 - finished views for esg-manger models - finished the html templates  for esg-manager  - finished the urls from esg-manager models
    v 0.8.1
    - finished views for esg-manger models
    - finished the html templates  for esg-manager
    - finished the urls from esg-manager models

--- esgmanager/urls.py
+++ esgmanager/urls.py
@@ -6,5 +6,28 @@ urlpatterns = [path('pillars/', views.ListViewPillars.as_view(), name='pillars')
                path('create-pillar/', views.CreateViewPillars.as_view(), name='create_pillar'),
                path('update-pillar/<uuid:pk>/', views.UpdateViewPillars.as_view(), name='update_pillar'),
                path('delete-pillar/<uuid:pk>/', views.DeleteViewPillars.as_view(), name='delete_pillar'),
-               path('esg-nav-form-pillar',views.NavFormPillars.as_view(),name='esg-nav-form-pillar'),
-               path('esg-nav-form-action-plan',views.NavFormActionPlan.as_view(),name='esg-nav-forms-action-plan')]
+
+               path('action-plans/', views.ListViewActionPlan.as_view(), name='action_plans'),
+               path('action-plan/<uuid:pk>/', views.DetailViewActionPlan.as_view(), name='action_plan'),
+               path('create-action-plan/', views.CreateViewActionPlan.as_view(), name='create_action_plan'),
+               path('update-action-plan/<uuid:pk>/', views.UpdateViewActionPlan.as_view(), name='update_action_plan'),
+               path('delete-action-plan/<uuid:pk>/', views.DeleteViewActionPlan.as_view(), name='delete_action_plan'),
+
+               path('objectives/', views.ListViewObjectives.as_view(), name='objectives'),
+               path('objective/<uuid:pk>/', views.DetailViewObjectives.as_view(), name='objective'),
+               path('create-objective/', views.CreateViewObjective.as_view(), name='create_objective'),
+               path('update-objective/<uuid:pk>/', views.UpdateViewObjectives.as_view(), name='update_objective'),
+               path('delete-objective/<uuid:pk>/', views.DeleteViewObjectives.as_view(), name='delete_objective'),
+
+               path('actions/', views.ListViewActions.as_view(), name='actions'),
+               path('action/<uuid:pk>/', views.DetailViewActions.as_view(), name='action'),
+               path('create-action/', views.CreateViewActions.as_view(), name='create_action'),
+               path('update-action/<uuid:pk>/', views.UpdateViewActions.as_view(), name='update_action'),
+               path('delete-action/<uuid:pk>/', views.DeleteViewActions.as_view(), name='delete_action'),
+
+               path('esg-nav-form-pillar', views.NavFormPillars.as_view(), name='esg_nav_form_pillar'),
+               path('esg-nav-form-action-plan', views.NavFormActionPlan.as_view(), name='esg_nav_forms_action_plan'),
+               path('esg-nav-form-objectives', views.NavFormObjectives.as_view(), name='esg_nav_forms_objectives'),
+               path('esg-nav-form-actions', views.NavFormActions.as_view(), name='esg_nav_forms_actions'),
+
+               ]
---
+++ esgmanager/views/objectives_views.py
@@ -0,0 +1,43 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from envdata.mixins import UpdateModeMixin, CompanyContextMixin
+from esgmanager.models import ESGActionPlanObjectives
+from esgmanager.forms import ESGActionPlanObjectivesForm
+
+
+class ListViewObjectives(LoginRequiredMixin, CompanyContextMixin, ListView):
+    model = ESGActionPlanObjectives
+    template_name = 'esgmanager/action_plan_objectives/objectives.html'
+    context_object_name = 'objectives'
+
+
+class DetailViewObjectives(LoginRequiredMixin, CompanyContextMixin, DetailView):
+    model = ESGActionPlanObjectives
+    template_name = 'esgmanager/action_plan_objectives/objective.html'
+    context_object_name = 'objective'
+
+
+class CreateViewObjective(LoginRequiredMixin, CompanyContextMixin, CreateView):
+    form_class = ESGActionPlanObjectivesForm
+    template_name = 'esgmanager/action_plan_objectives/form-objective.html'
+    success_url = reverse_lazy('objectives')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user.profile
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('objectives')
+
+
+class UpdateViewObjectives(LoginRequiredMixin, CompanyContextMixin, UpdateView, UpdateModeMixin):
+    form_class = ESGActionPlanObjectivesForm
+    template_name = 'esgmanager/action_plan_objectives/form-objective.html'
+    success_url = reverse_lazy('objectives')
+
+
+class DeleteViewObjectives(LoginRequiredMixin, CompanyContextMixin, DeleteView):
+    model = ESGActionPlanObjectives
+    template_name = 'esgmanager/delete-universal.html'
+    success_url = reverse_lazy('objectives')
