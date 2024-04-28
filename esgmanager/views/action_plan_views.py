3171e382add264e7575f237000941a43bd766f26
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-04-28 10:51:26 +0300

    working on the action plan views and html templates
    working on the action plan views and html templates

--- esgmanager/views/action_plan_views.py
+++ esgmanager/views/action_plan_views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Prefetch
 from envdata.mixins import CompanyContextMixin, UpdateModeMixin
-from esgmanager.models import ESGActionPlan
+from esgmanager.models import ESGActionPlan, ESGActionPlanObjectives
 from esgmanager.forms import ESGActionPlanForm
 
 
@@ -11,11 +12,25 @@ class ListViewActionPlan(LoginRequiredMixin, CompanyContextMixin, ListView):
     template_name = 'esgmanager/action_plan/action-plans.html'
     context_object_name = 'action_plans'
 
+    def get_queryset(self):
+        objectives_prefetched = Prefetch('esgactionplanobjectives_set',
+                                         queryset=ESGActionPlanObjectives.objects.all().prefetch_related(
+                                             'esgactionplanactions_set'))
+        queryset = super().get_queryset().select_related('company', 'pillar').prefetch_related(objectives_prefetched)
+        return queryset
+
 
 class DetailViewActionPlan(LoginRequiredMixin, CompanyContextMixin, DetailView):
     model = ESGActionPlan
     template_name = 'esgmanager/action_plan/action-plan.html'
     context_object_name = 'action_plan'
+
+    def get_object(self):
+        queryset = super().get_queryset()
+        queryset = queryset.select_related('company', 'pillar').prefetch_related(
+            'esgactionplanobjectives_set__esgactionplanactions_set'
+        )
+        return queryset.get(pk=self.kwargs.get('pk'))
 
 
 class CreateViewActionPlan(LoginRequiredMixin, CompanyContextMixin, CreateView):
