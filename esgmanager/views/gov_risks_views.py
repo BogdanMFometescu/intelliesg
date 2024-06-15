19869b3917221c2f988416e73918675c20b99983
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-06-15 22:00:49 +0300

    updated taxonomy html templates and risks templates
    updated taxonomy html templates and risks templates

--- esgmanager/views/gov_risks_views.py
+++ esgmanager/views/gov_risks_views.py
@@ -9,36 +9,36 @@ from django.urls import reverse_lazy
 class GovernanceRiskListView(LoginRequiredMixin, CompanyContextMixin, ListView):
     model = GovernanceRisks
     template_name = 'esgmanager/risks/governance/gov-risks.html'
-    context_object_name = 'governance_risks'
+    context_object_name = 'gov_risks'
 
 
 class GovernanceRiskDetailView(LoginRequiredMixin, CompanyContextMixin, DetailView):
     model = GovernanceRisks
     template_name = 'esgmanager/risks/governance/gov-risk.html'
-    context_object_name = 'governance_risk'
+    context_object_name = 'gov_risk'
 
 
 class GovernanceRiskCreateView(LoginRequiredMixin, CompanyContextMixin, CreateView):
     form_class = GovernanceRisksForm
     template_name = 'esgmanager/risks/governance/form-gov-risk.html'
-    success_url = reverse_lazy('governance_risks')
+    success_url = reverse_lazy('gov_risks')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user.profile
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('governance_risks')
+        return reverse_lazy('gov_risks')
 
 
 class GovernanceRiskUpdateView(LoginRequiredMixin, CompanyContextMixin, UpdateView, UpdateModeMixin):
     model = GovernanceRisks
     form_class = GovernanceRisksForm
     template_name = 'esgmanager/risks/governance/form-gov-risk.html'
-    success_url = reverse_lazy('governance_risks')
+    success_url = reverse_lazy('gov_risks')
 
 
 class GovernanceRiskDeleteView(LoginRequiredMixin, CompanyContextMixin, DeleteView):
     model = GovernanceRisks
     template_name = 'envdata/delete-universal.html'
-    success_url = reverse_lazy('governance_risks')
+    success_url = reverse_lazy('gov_risks')
--- esgmanager/views/soc_risks_views.py
+++ esgmanager/views/soc_risks_views.py
@@ -9,36 +9,36 @@ from django.urls import reverse_lazy
 class SocialRiskListView(LoginRequiredMixin, CompanyContextMixin, ListView):
     model = SocialRisk
     template_name = 'esgmanager/risks/social/soc-risks.html'
-    context_object_name = 'social_risks'
+    context_object_name = 'soc_risks'
 
 
 class SocialRiskDetailView(LoginRequiredMixin, CompanyContextMixin, DetailView):
     model = SocialRisk
     template_name = 'esgmanager/risks/social/soc-risk.html'
-    context_object_name = 'social_risk'
+    context_object_name = 'soc_risk'
 
 
 class SocialRiskCreateView(LoginRequiredMixin, CompanyContextMixin, CreateView):
     form_class = SocialRisksForm
     template_name = 'esgmanager/risks/social/form-soc-risk.html'
-    success_url = reverse_lazy('social_risks')
+    success_url = reverse_lazy('soc_risks')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user.profile
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('social_risks')
+        return reverse_lazy('soc_risks')
 
 
 class SocialRiskUpdateView(LoginRequiredMixin, CompanyContextMixin, UpdateView, UpdateModeMixin):
     model = SocialRisk
     form_class = SocialRisksForm
     template_name = 'esgmanager/risks/social/form-soc-risk.html'
-    success_url = reverse_lazy('social_risks')
+    success_url = reverse_lazy('soc_risks')
 
 
 class SocialRiskDeleteView(LoginRequiredMixin, CompanyContextMixin, DeleteView):
     model = SocialRisk
     template_name = 'envdata/delete-universal.html'
-    success_url = reverse_lazy('social_risks')
+    success_url = reverse_lazy('soc_risks')
