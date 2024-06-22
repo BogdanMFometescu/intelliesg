6fa3d193ddc682f4c8b7a5ddb9a6ae526c592141
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-06-22 21:19:59 +0300

    updated yml file
    updated yml file

--- intelliesg/production.py
+++ intelliesg/production.py
@@ -2,16 +2,13 @@ from settings import *
 from settings import BASE_DIR
 import os
 
-
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
-#CORS_ALLOWED_ORIGINS = []
-SECRET_KEY = os.environ['MY_SECRET_KEY']
-
+# CORS_ALLOWED_ORIGINS = []
+SECRET_KEY = os.environ['SECRET_KEY']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -25,8 +22,7 @@ MIDDLEWARE = [
 ]
 
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
-
+CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
 
 DATABASES = {
     'default': {
@@ -34,11 +30,9 @@ DATABASES = {
         'NAME': CONNECTION_STR['dbname'],
         'HOST': CONNECTION_STR['host'],
         'USER': CONNECTION_STR['user'],
-        'PASSWORD':CONNECTION_STR['password'] ,
-
+        'PASSWORD': CONNECTION_STR['password'],
 
     }
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
