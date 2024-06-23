c0cf9f28245577d5c797712d1a82c6a9a9a644b1
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-06-23 10:53:31 +0300

    updated production.py
    updated production.py

--- intelliesg/production.py
+++ intelliesg/production.py
@@ -2,16 +2,27 @@ from settings import *
 from settings import BASE_DIR
 import os
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+# Retrieve environment variables
+WEBSITE_HOSTNAME = os.environ.get('WEBSITE_HOSTNAME', '')
+
+# Set allowed hosts and CSRF trusted origins
+ALLOWED_HOSTS = [WEBSITE_HOSTNAME] if WEBSITE_HOSTNAME else []
+CSRF_TRUSTED_ORIGINS = [f'https://{WEBSITE_HOSTNAME}'] if WEBSITE_HOSTNAME else []
+
+# Debug mode
 DEBUG = False
-SECRET_KEY = os.environ['SECRET_KEY']
 
+# Secret key
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# Static files settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Middleware settings
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -20,18 +31,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')}
+# Parse connection string
+CONNECTION = os.environ.get('AZURE_POSTGRESQL_CONNECTIONSTRING', '')
+CONNECTION_STR = {pair.split('=')[0]: pair.split('=')[1] for pair in CONNECTION.split(' ')} if CONNECTION else {}
 
+# Database settings
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': CONNECTION_STR['dbname'],
-        'HOST': CONNECTION_STR['host'],
-        'USER': CONNECTION_STR['user'],
-        'PASSWORD': CONNECTION_STR['password'],
-
+        'NAME': CONNECTION_STR.get('dbname', ''),
+        'HOST': CONNECTION_STR.get('host', ''),
+        'USER': CONNECTION_STR.get('user', ''),
+        'PASSWORD': CONNECTION_STR.get('password', ''),
     }
 }
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
