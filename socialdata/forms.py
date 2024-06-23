11a708b0c93353d5e4d6e6056228e518230cbe48
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-06-23 17:54:33 +0300

    clean up migrations
    clean up migrations

--- socialdata/forms.py
+++ socialdata/forms.py
@@ -4,12 +4,22 @@ from django.forms import ModelForm
 
 class BaseSocialForm(ModelForm):
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         super(BaseSocialForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'form-control'})
+
+    def save(self, commit=True):
+        instance = super(BaseSocialForm, self).save(commit=False)
+        if self.user:
+            instance.profile = self.user.profile
+        if commit:
+            instance.save()
+        return instance
 
 
 class HealthAndSafetyForm(BaseSocialForm):
     class Meta:
         model = HealthAndSafety
         fields = '__all__'
+        exclude =['profile']
