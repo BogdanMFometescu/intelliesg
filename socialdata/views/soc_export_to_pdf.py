6c83d989284eb4762dab89ae3bece9e8191981e8
Bogdan Fometescu <75015831+BogdanMFometescu@users.noreply.github.com>
2024-07-09 18:49:57 +0300

    added pdf export for employee age model
    added pdf export for employee age model

--- socialdata/views/soc_export_to_pdf.py
+++ socialdata/views/soc_export_to_pdf.py
@@ -5,8 +5,8 @@ from xhtml2pdf import pisa
 from django.http import HttpResponse
 from django.db.models import Sum, F
 from django.template.loader import render_to_string
-from .filters import HealthAndSafetyFilter, EmployeeContractsFilter
-from socialdata.models import HealthAndSafety, EmployeeByContracts
+from .filters import HealthAndSafetyFilter, EmployeeContractsFilter, NewEmployeeByAgeFilter
+from socialdata.models import HealthAndSafety, EmployeeByContracts, NewEmployeeByAge
 
 
 class HealthAndSafetyPdfView(LoginRequiredMixin, CompanyContextMixin, View):
@@ -61,12 +61,47 @@ class EmployeeContractsPdfView(LoginRequiredMixin, CompanyContextMixin, View):
 
         html_string = render_to_string('socialdata/export_to_pdf/contracts-report.html',
                                        {'total_company_contracts': filtered_hs,
-                                           'total_full_time': filtered_full_time_contracts,
+                                        'total_full_time': filtered_full_time_contracts,
                                         'total_fixed_term': filtered_fixed_term_contracts,
                                         'total_part_time': filtered_part_time_contracts,
                                         'total_contracts': filtered_total_contracts})
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = 'attachment;filename=employee_contracts_report.pdf'
+
+        pisa_status = pisa.CreatePDF(html_string, dest=response)
+        if pisa_status.err:
+            return HttpResponse('We had some errors. Please try again' + html_string + '</pre>')
+        return response
+
+
+class EmployeeAgePdfView(LoginRequiredMixin, CompanyContextMixin, View):
+    def get(self, request, *args, **kwargs):
+        filter_set = NewEmployeeByAgeFilter(request.GET, queryset=NewEmployeeByAge.objects.all())
+        filtered_hs = filter_set.qs
+        filtered_under_30 = \
+            filtered_hs.annotate(total_under_30=F('men_under_30') + F('women_under_30')).aggregate(
+                total_emp_under_30=Sum('total_under_30'))['total_emp_under_30'] or 0
+
+        filtered_between_30_and_50 = filtered_hs.annotate(
+            total_between_30_and_50=F('men_between_30_and_50') + F('women_between_30_and_50')).aggregate(
+            total_30_and_50=Sum('total_between_30_and_50'))['total_30_and_50'] or 0
+
+        filtered_over_50 = filtered_hs.annotate(total_over_50=F('men_over_50') + F('women_over_50')).aggregate(
+            total_over=Sum('total_over_50'))['total_over'] or 0
+
+        filtered_total_employees_by_age = filtered_hs.annotate(
+            total_employees=F('men_under_30') + F('women_under_30') + F('men_between_30_and_50') + F(
+                'women_between_30_and_50') + F('men_over_50') + F('women_over_50')).aggregate(
+            total=Sum('total_employees'))['total'] or 0
+
+        html_string = render_to_string('socialdata/export_to_pdf/employees-by-age-report.html',
+                                       {'total_age_filtered': filtered_hs,
+                                        'total_under_30': filtered_under_30,
+                                        'total_between_30_and_50': filtered_between_30_and_50,
+                                        'total_over_50': filtered_over_50,
+                                        'total_age_sum': filtered_total_employees_by_age})
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment;filename=age_report.pdf'
 
         pisa_status = pisa.CreatePDF(html_string, dest=response)
         if pisa_status.err:
